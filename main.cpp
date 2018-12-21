5c76838676bf4b461f6d24baef8b6184b400deb8
Adrien Wartelle <adrilord@gmail.com>
2018-12-21 17:56:34 +0100

    simplify memory management (just static arrays)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <cstring>
 #include <iostream>
 #include <fstream>
 #include <limits>
@@ -9,25 +10,36 @@
 using namespace std;
 
 #define MAX_TIME 1000
+#define J_MAX 100 //MAX number of jobs
+#define O_MAX 10 //MAX number of operations per jobs
+#define M_MAX 100 //MAX number of machines
+#define L_MAX 10 //MAX number of maintenance tasks per machine
+#define SEED 2018
 
-struct parameters{
+struct Parameters{
     int n=0; //number of jobs
     int m=0; //number of machines
-    int* n_o; //number of operations of jobs i 
-    int* L; //number of maintenance tasks on machine j
-    bool*** A; //availables machines for task k of job i
-    double*** t; //processing time for task k of job i on machine j
-    double** p; //processing time for maintenance task l on machine j
-    double** t_e; //maintenance completion time windows begin (earliest)
-    double** t_l; //maintenance completion time windows end (lateness)
+    int n_o[J_MAX]; //number of operations of jobs i 
+    int L[M_MAX]; //number of maintenance tasks on machine j
+    bool A[J_MAX][O_MAX][M_MAX]; //availables machines for task k of job i
+    double t[J_MAX][O_MAX][M_MAX]; //processing time for task k of job i on machine j
+    double p[L_MAX][M_MAX]; //processing time for maintenance task l on machine j
+    double t_e[L_MAX][M_MAX]; //maintenance completion time windows begin (earliest)
+    double t_l[L_MAX][M_MAX]; //maintenance completion time windows end (lateness)
 };
 
-//~ struct variables{
+struct Variables{
     //~ double** y; //completion time of of maintenance task jl
-    //~ bool*** x; //affectation of the operation ik to machine j
-//~ };
+    bool x[L_MAX][M_MAX]; //affectation of the operation ik to machine j
+};
 
-int read_parameters(string filename, parameters& params)
+
+//static variables
+static MTRand rand_gen(SEED);
+static Parameters params;
+static Variables vars; // assignements
+
+int read_parameters(string filename)
 {
     ifstream file(filename);
     if(file.fail()) {
@@ -41,21 +53,16 @@ int read_parameters(string filename, parameters& params)
     cout << "Got n : " << params.n << endl;
     file >> params.m;
     cout << "Got m : " << params.m << endl;
-    params.n_o = new int[params.n];
     for(int i = 0; i<params.n; i++) {
        file >> params.n_o[i];
        cout << "Got n_o["<<i<<"] : " << params.n_o[i] << endl;
     }
-    params.L = new int[params.m];
     for(int j = 0; j<params.m; j++) {
         file >> params.L[j];
         cout << "Got L["<<j<<"] : " << params.L[j] << endl;
     }
-    params.A = new bool**[params.n];
     for(int i=0; i<params.n; i++) {
-        params.A[i] = new bool*[params.n_o[i]];
         for(int k=0; k<params.n_o[i]; k++) {
-            params.A[i][k] = new bool[params.m];
             for(int j=0; j<params.m; j++) {
                 string temp_word;
                 file >> temp_word;
@@ -68,11 +75,8 @@ int read_parameters(string filename, parameters& params)
             }
         }
     }
-    params.t = new double**[params.n];
     for(int i=0; i<params.n; i++) {
-        params.t[i] = new double*[params.n_o[i]];
         for(int k=0; k<params.n_o[i]; k++) {
-            params.t[i][k] = new double[params.m];
             for(int j=0; j<params.m; j++)
             {
                 file >> params.t[i][k][j];
@@ -80,25 +84,19 @@ int read_parameters(string filename, parameters& params)
             }
         }
     }
-    params.p = new double*[params.m];
     for(int j=0; j<params.m; j++) {
-        params.p[j] = new double[params.L[j]];
         for(int l=0; l<params.L[j]; l++) {
             file >> params.p[j][l];
             cout << "Got p["<<j<<"]["<<l<<"] : " << params.p[j][l] << endl;
         }
     }
-    params.t_e = new double*[params.m];
     for(int j=0; j<params.m; j++) {
-        params.t_e[j] = new double[params.L[j]];
         for(int l=0; l<params.L[j]; l++) {
             file >> params.t_e[j][l];
             cout << "Got t_e["<<j<<"]["<<l<<"] : " << params.t_e[j][l] << endl;
         }
     }
-    params.t_l = new double*[params.m];
     for(int j=0; j<params.m; j++) {
-        params.t_l[j] = new double[params.L[j]];
         for(int l=0; l<params.L[j]; l++) {
             file >> params.t_l[j][l];
             cout << "Got t_l["<<j<<"]["<<l<<"] : " << params.t_l[j][l] << endl;
@@ -107,53 +105,10 @@ int read_parameters(string filename, parameters& params)
     return 0;
 }
 
-void delete_parameters(parameters& params)
-{
-    free(params.n_o);
-    free(params.L);
+void initialize_x(bool* x[O_MAX][M_MAX])
+{ 
     for(int i=0; i<params.n; i++) {
         for(int k=0; k<params.n_o[i]; k++) {
-            free(params.A[i][k]);
-        }
-        free(params.A[i]);
-    }
-    free(params.A);
-    for(int i=0; i<params.n; i++) {
-        for(int k=0; k<params.n_o[i]; k++) {
-            free(params.t[i][k]);
-        }
-        free(params.t[i]);
-    }
-    free(params.t);
-    for(int j=0; j<params.m; j++) {
-        free(params.p[j]);
-    }
-    free(params.p);
-    for(int j=0; j<params.m; j++) {
-        free(params.t_e[j]);
-    }
-    free(params.t_e);
-    for(int j=0; j<params.m; j++) {
-        free(params.t_l[j]);
-    }
-    free(params.t_l);
-}
-
-    //~ vars.y = new double*[params.m];
-    //~ for(int j=0; j<params.n; j++) {
-        //~ vars.y[j] = new double[params.L[j]];
-        //~ for(int l=0; l<params.L[j]; l++) {
-            //~ vars.y[j][l] = 0;
-        //~ }
-    //~ }
-
-void initialize_x(bool*** x, parameters& params)
-{
-    x = new bool**[params.n]; 
-    for(int i=0; i<params.n; i++) {
-        x[i] = new bool*[params.n_o[i]];
-        for(int k=0; k<params.n_o[i]; k++) {
-            x[i][k] = new bool[params.m];
             for(int j=0; j<params.m; j++) {
                     x[i][k][j]=false;
             }
@@ -161,31 +116,13 @@ void initialize_x(bool*** x, parameters& params)
     }
 }
 
-//~ for(int j=0; j<params.n; j++) {
-        //~ free(vars.y[j]);
-    //~ }
-    //~ free(vars.y);
-
-void delete_x(bool*** x, parameters& params)
+void GRASP_routing(int nb_iters)
 {
-    for(int i=0; i<params.n; i++) {
-        for(int k=0; k<params.n_o[i]; k++) {
-            free(x[i][k]);
-        }
-        free(x[i]);
-    }
-    free(x);
-}
-
-bool*** GRASP_routing(MTRand rand_gen, int* alpha, int nb_iters, parameters& params)
-{
-    bool*** x_sol=NULL;
     double W_tot=numeric_limits<double>::max(); // OFV : total workload
-    //bool
+    bool x_temp[J_MAX][O_MAX][M_MAX];
     for(int t=0; t<nb_iters; t++) {
-        bool*** x_temp;
+        initialize_x(x_temp);
         double W_tot_temp=0;
-        initialize_x(x_temp,params);
         for(int i=0; i<params.n; i++) {
             for(int k=0; k<params.n_o[i]; k++) {
                 //Restreined Candidates List (will be sorted)
@@ -208,7 +145,7 @@ bool*** GRASP_routing(MTRand rand_gen, int* alpha, int nb_iters, parameters& par
                     }
                 }
                 range = maxW - minW;
-                alpha = rand_gen.randDblExc(); //NO
+                alpha = rand_gen.randDblExc(); //NO ?
                 width = range * alpha;
                 for(int j=0; j<params.m; j++) {
                     if(params.t[i][k][j] <= minW + width) {
@@ -223,10 +160,10 @@ bool*** GRASP_routing(MTRand rand_gen, int* alpha, int nb_iters, parameters& par
                     proba[r]=1/r;
                     totalBias+=1/r;
                 }
-                for(unsigned int r=0; r<proba.size(); r++) {
-                    proba[r]/=totalBias;
-                }
-                mach_runif = rand_gen.randDblExc();
+                //~ for(unsigned int r=0; r<proba.size(); r++) {
+                    //~ proba[r]/=totalBias;
+                //~ }
+                mach_runif = rand_gen.randDblExc(totalBias);
                 for(unsigned int r=0; r<proba.size(); r++) {
                     if(mach_runif <= proba[r]) {
                         x_temp[i][k][index_RCL[r]] = true;
@@ -239,23 +176,28 @@ bool*** GRASP_routing(MTRand rand_gen, int* alpha, int nb_iters, parameters& par
             }
         }
         if(t==0) {
-            x_sol = x_temp;
+            memcpy(vars.x, x_temp, sizeof(vars.x));
         } else if (W_tot_temp < W_tot) {
-            delete_x(x_sol,params);
-            x_sol = x_temp;
-        } else {
-            delete_x(x_temp,params);
+            memcpy(vars.x, x_temp, sizeof(vars.x));
+            W_tot = W_tot_temp;
         }
     }
-    return x_sol;
 }
 
 int main()
 {
-    parameters params;
-    read_parameters("problem8x8.in",params);
-    bool*** x;
-    delete_parameters(params);
+    read_parameters("problem8x8.in");
+    GRASP_routing(5);
+    for(int i=0; i<params.n; i++) {
+        cout << i << " ";
+        for(int j=0; j<params.m; j++) {
+            cout << j << " ";
+            for(int k=0; k<params.n_o[i]; k++) {
+                    cout << vars.x[i][k][j];
+            }
+            cout << endl;
+        }
+    }
     // decision varaibles -> c, y, x
     return 0;
 }
